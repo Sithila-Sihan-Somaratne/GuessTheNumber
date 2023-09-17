672c32219052255634b2bd36ef70bcf0065e61d3
Sithila2010 <chutimalli182@gmail.com>
2023-09-17 18:24:56 +0200

    Modify the GuessTheNumber game with adding score and max score

--- src/main/java/AppInitializer.java
+++ src/main/java/AppInitializer.java
@@ -1,10 +1,7 @@
-package app;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
-import java.io.IOException;
 import java.util.Objects;
 
 public class AppInitializer extends Application {
@@ -17,8 +14,11 @@ public class AppInitializer extends Application {
     public void start(Stage stage) {
         stage.setResizable(false);
         try {
-            stage.setScene(new Scene(FXMLLoader.load(Objects.requireNonNull(getClass().getResource("../view/guessNumberForm.fxml")))));
-        } catch (IOException e) {
+            System.err.close();
+            System.setErr(System.out);
+            stage.setScene(new Scene(FXMLLoader.load(Objects.requireNonNull(getClass().getResource("view/guessNumberForm.fxml")))));
+            stage.show();
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
--- src/main/java/controller/GuessNumberFormController.java
+++ src/main/java/controller/GuessNumberFormController.java
@@ -5,19 +5,30 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.geometry.Pos;
 import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
 import dto.NumberGuessAndGenerator;
 import javafx.scene.paint.Color;
 
+import java.util.Optional;
 import java.util.Random;
 
 public class GuessNumberFormController {
-
+    Random r = new Random();
+    int generatedNumber = r.nextInt(101);
+    int buttonClick = 0;
+    int points;
     @FXML
     private JFXTextField txtGuess;
 
     @FXML
     private Label txtOutput;
+
+    @FXML
+    private Label txtPoints;
+
+    @FXML
+    private Label txtMaxScore;
 
     @FXML
     void exit(ActionEvent ignoredEvent) {
@@ -26,35 +37,63 @@ public class GuessNumberFormController {
 
     @FXML
     void guessTheNumber(ActionEvent ignoredEvent) {
-        Random r = new Random(101);
-        int generatedNumber = r.nextInt();
         NumberGuessAndGenerator numberGuessAndGenerator = new NumberGuessAndGenerator();
-        try{
-            numberGuessAndGenerator.setGuessedNumber(Integer.parseInt(txtGuess.getText()));
-        }catch(Exception e){
-            new Alert(Alert.AlertType.ERROR,"You didn't enter a number!").show();
-            new Alert(Alert.AlertType.ERROR, String.valueOf(e)).show();
-        }
-        numberGuessAndGenerator.setGeneratedNumber(generatedNumber);
-        txtOutput.setTextFill(Color.rgb(255,255,255));
-        txtOutput.setAlignment(Pos.CENTER);
-        if(numberGuessAndGenerator.getGuessedNumber() != numberGuessAndGenerator.getGeneratedNumber()){
-            txtOutput.setStyle("-fx-background-color: #e35c5c; -fx-font-weight: BOLD");
-            if(numberGuessAndGenerator.getGeneratedNumber() > numberGuessAndGenerator.getGuessedNumber()){
-                txtOutput.setText("The number you tried to guess is too high. Please try again.");
-            }else {
-                txtOutput.setText("The number you tried to guess is too low. Please try again.");
+        do {
+            try {
+                if(Integer.parseInt(txtGuess.getText()) > 100 || Integer.parseInt(txtGuess.getText()) < 0){
+                    new Alert(Alert.AlertType.ERROR, "Number should be greater than -1 and less than 101").show();
+                    break;
+                }
+                points = Integer.parseInt(txtPoints.getText());
+                numberGuessAndGenerator.setGuessedNumber(Integer.parseInt(txtGuess.getText()));
+                numberGuessAndGenerator.setGeneratedNumber(generatedNumber);
+                System.out.println(generatedNumber);
+                txtOutput.setTextFill(Color.rgb(255, 255, 255));
+                txtOutput.setAlignment(Pos.CENTER);
+                buttonClick++;
+                if(buttonClick != 6){
+                    if (numberGuessAndGenerator.getGuessedNumber() == numberGuessAndGenerator.getGeneratedNumber()) {
+                        txtOutput.setStyle("-fx-background-color: #347740; -fx-font-weight: BOLD");
+                        txtOutput.setText("Great! You guessed the number! Now, try to guess the next one.");
+                        generatedNumber = r.nextInt(101);
+                        points = points+3;
+                        txtPoints.setText(String.valueOf(points));
+                        txtMaxScore.setText(String.valueOf(points));
+                        buttonClick = 0;
+                    }else{
+                        txtOutput.setStyle("-fx-background-color: #e35c5c; -fx-font-weight: BOLD");
+                        if (numberGuessAndGenerator.getGeneratedNumber() > numberGuessAndGenerator.getGuessedNumber()) {
+                            txtOutput.setText("The number you tried to guess is too low. Please try again.");
+                        } else {
+                            txtOutput.setText("The number you tried to guess is too high. Please try again.");
+                        }
+                    }
+                }else{
+                    Optional<ButtonType> buttonType = new Alert(Alert.AlertType.CONFIRMATION,"Sorry, you have exhausted 5 trials. The number was "+generatedNumber+". \nDo you want to retry the game?", ButtonType.YES, ButtonType.NO).showAndWait();
+                    if(buttonType.get() == ButtonType.YES){
+                        refreshTheWindow();
+                        generatedNumber = r.nextInt(101);
+                    }
+                }
+                break;
+            } catch (Exception e) {
+                new Alert(Alert.AlertType.ERROR, "You didn't enter a number!").show();
+                break;
             }
-        }else{
-            txtOutput.setStyle("-fx-background-color: #347740; -fx-font-weight: BOLD");
-            txtOutput.setText("Great! You guessed the number!");
-        }
+        } while (true);
+
     }
 
     @FXML
     void refreshTheWindow(ActionEvent ignoredEvent) {
-        txtGuess.setText("");
-        txtOutput.setText("");
+        refreshTheWindow();
     }
 
+    private void refreshTheWindow() {
+        txtPoints.setText("0");
+        txtMaxScore.setText("0");
+        txtGuess.setText("");
+        txtOutput.setStyle("-fx-background-color: #fff; -fx-font-weight: BOLD");
+        txtOutput.setText("");
+    }
 }
